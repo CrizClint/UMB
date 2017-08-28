2d5b49f99b2ecf92e03109ad74b6cf631d99c198
Clint Mathews <mathewsclint28@gmail.com>
2017-08-29 00:39:45 +0530

    Initial commit

---
+++ app/src/test/java/in/unaisemoneybank/umb/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package in.unaisemoneybank.umb;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <ic_launcher href="http://d.android.com/tools/testing">Testing documentation</ic_launcher>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
